5568d183b62e6910d029f83feba31b00d2b48e68
brianush1 <brianush1@outlook.com>
2019-02-16 09:22:56 -0500

    Fix several security vulnerabilities and refactor several parts of the source code

--- init.lua
+++ init.lua
@@ -1,14 +1,23 @@
 -- @name            Ambassador Library
 -- @author          brianush1
 -- @description     A library that allows for easy communication between client and server
--- @version         0.07
+-- @version         0.08
 
 --[[
 	Changelog:
+	
+	0.08:
+		- Fix several security vulnerabilities
+		- Refactor several parts of the source code
+			- Add Ambassador.DataReceived signal
+				Server: Event Ambassador.DataReceived(Player player, string name, data)
+				Client: Event Ambassador.DataReceived(string name, data)
+			- Remove unnecessary encryption
+			- Fix typo in "transmittion" to "transmission"
 
 	0.07:
 		- Critical bugfix
-	
+
 	0.06:
 		- Allow passing instances
 
@@ -26,161 +35,79 @@
 		  e.g. object A transmitted Client -> Server -> Client != object A
 ]]
 
+-- Signal library
+
+function Signal()
+	local this = {}
+
+	local mBindableEvent = Instance.new('BindableEvent')
+	local mAllCns = {} --all connection objects returned by mBindableEvent::connect
+
+	--main functions
+	function this:connect(func)
+		if self ~= this then error("connect must be called with `:`, not `.`", 2) end
+		if type(func) ~= 'function' then
+			error("Argument #1 of connect must be a function, got a "..type(func), 2)
+		end
+		local pubCn = {}
+		function pubCn:disconnect()
+			mAllCns[pubCn] = nil
+		end
+		pubCn.Disconnect = pubCn.disconnect
+		mAllCns[pubCn] = function(...)
+			pcall(function(...) coroutine.wrap(func)(...) end, ...)
+		end
+		
+		return pubCn
+	end
+	
+	function this:disconnect()
+		if self ~= this then error("disconnect must be called with `:`, not `.`", 2) end
+		for cn, _ in pairs(mAllCns) do
+			mAllCns[cn] = nil
+		end
+	end
+	
+	function this:wait()
+		if self ~= this then error("wait must be called with `:`, not `.`", 2) end
+		local bindable = Instance.new("BindableEvent")
+		
+		local len, data
+		local cn = this:connect(function(...)
+			len = select("#", ...)
+			data = {...}
+			bindable:Fire()
+		end)
+		
+		bindable.Event:Wait()
+		bindable:Destroy()
+		
+		if len ~= nil then
+			return unpack(data, 1, len)
+		else
+			return
+		end
+	end
+	
+	this.Connect = this.connect
+	this.Disconnect = this.disconnect
+	this.Wait = this.wait
+
+	return this, function(...)
+		for cn, fn in pairs(mAllCns) do
+			fn(...)
+		end
+	end
+end
+
+-- End Signal library
+
 local Ambassador = {}
 
-local Server = game:GetService("RunService"):IsServer()
-
-function getName(server, name, target)
-	return (server and "Server" or "Client") .. "Ambassador/" .. name .. "/" .. target.Name
-end
-
-local InvokationType = {
-	RequestAmbassador = 0
-}
-
-local key = "builtinAMQLFPXMFTWLkey"
-
---[[
-
-Encryption/Decryption
-By: NoliCAIKS
-Source: https://wiki.roblox.com/index.php?title=User:NoliCAIKS/Code/Encryption
-
-]]
-function decrypt(cipher, key)
-	local key_bytes
-	if type(key) == "string" then
-		key_bytes = {}
-		for key_index = 1, #key do
-			key_bytes[key_index] = string.byte(key, key_index)
-		end
-	else
-		key_bytes = key
-	end
-	local cipher_raw_length = #cipher
-	local key_length = #key_bytes
-	local cipher_bytes = {}
-	local cipher_length = 0
-	local cipher_bytes_index = 1
-	for byte_str in string.gmatch(cipher, "%x%x") do
-		cipher_length = cipher_length + 1
-		cipher_bytes[cipher_length] = tonumber(byte_str, 16)
-	end
-	local random_bytes = {}
-	local random_seed = 0
-	for key_index = 1, key_length do
-		random_seed = (random_seed + key_bytes[key_index] * key_index) * 1103515245 + 12345
-		random_seed = (random_seed - random_seed % 65536) / 65536 % 4294967296
-	end
-	for random_index = 1, (cipher_length - key_length + 1) * key_length do
-		random_seed = (random_seed % 4194304 * 1103515245 + 12345)
-		random_bytes[random_index] = (random_seed - random_seed % 65536) / 65536 % 256
-	end
-	local random_index = #random_bytes
-	local last_key_byte = key_bytes[key_length]
-	local result_bytes = {}
-	for cipher_index = cipher_length, key_length, -1 do
-		local result_byte = cipher_bytes[cipher_index] - last_key_byte
-		if result_byte < 0 then
-			result_byte = result_byte + 256
-		end
-		result_byte = result_byte - random_bytes[random_index]
-		random_index = random_index - 1
-		if result_byte < 0 then
-			result_byte = result_byte + 256
-		end
-		for key_index = key_length - 1, 1, -1 do
-			cipher_index = cipher_index - 1
-			local cipher_byte = cipher_bytes[cipher_index] - key_bytes[key_index]
-			if cipher_byte < 0 then
-				cipher_byte = cipher_byte + 256
-			end
-			cipher_byte = cipher_byte - result_byte
-			if cipher_byte < 0 then
-				cipher_byte = cipher_byte + 256
-			end
-			cipher_byte = cipher_byte - random_bytes[random_index]
-			random_index = random_index - 1
-			if cipher_byte < 0 then
-				cipher_byte = cipher_byte + 256
-			end
-			cipher_bytes[cipher_index] = cipher_byte
-		end
-		result_bytes[cipher_index] = result_byte
-	end
-	local result_characters = {}
-	for result_index = 1, #result_bytes do
-		result_characters[result_index] = string.char(result_bytes[result_index])
-	end
-	return table.concat(result_characters)
-end
-
-function encrypt(message, key)
-	local key_bytes
-	if type(key) == "string" then
-		key_bytes = {}
-		for key_index = 1, #key do
-			key_bytes[key_index] = string.byte(key, key_index)
-		end
-	else
-		key_bytes = key
-	end
-	local message_length = #message
-	local key_length = #key_bytes
-	local message_bytes = {}
-	for message_index = 1, message_length do
-		message_bytes[message_index] = string.byte(message, message_index)
-	end
-	local result_bytes = {}
-	local random_seed = 0
-	for key_index = 1, key_length do
-		random_seed = (random_seed + key_bytes[key_index] * key_index) * 1103515245 + 12345
-		random_seed = (random_seed - random_seed % 65536) / 65536 % 4294967296
-	end
-	for message_index = 1, message_length do
-		local message_byte = message_bytes[message_index]
-		for key_index = 1, key_length do
-			local key_byte = key_bytes[key_index]
-			local result_index = message_index + key_index - 1
-			local result_byte = message_byte + (result_bytes[result_index] or 0)
-			if result_byte > 255 then
-				result_byte = result_byte - 256
-			end
-			result_byte = result_byte + key_byte
-			if result_byte > 255 then
-				result_byte = result_byte - 256
-			end
-			random_seed = (random_seed % 4194304 * 1103515245 + 12345)
-			result_byte = result_byte + (random_seed - random_seed % 65536) / 65536 % 256
-			if result_byte > 255 then
-				result_byte = result_byte - 256
-			end
-			result_bytes[result_index] = result_byte
-		end
-	end
-	local result_buffer = {}
-	local result_buffer_index = 1
-	for result_index = 1, #result_bytes do
-		local result_byte = result_bytes[result_index]
-		result_buffer[result_buffer_index] = string.format("%02x", result_byte)
-		result_buffer_index = result_buffer_index + 1
-	end
-	return table.concat(result_buffer)
-end
-
-function createRemote(name)
-	if Server then
-		local remote = Instance.new("RemoteFunction")
-		remote.Name = name
-		remote.Parent = game:GetService("ReplicatedStorage")
-		return remote
-	else
-		return game:GetService("ReplicatedStorage"):WaitForChild("Ambassador/RemoteRequestHandler"):InvokeServer(name)
-	end
-end
+local IS_SERVER = game:GetService("RunService"):IsServer()
 
 function getRemote(name)
-	if Server then
+	if IS_SERVER then
 		local remote = game:GetService("ReplicatedStorage"):FindFirstChild("Ambassador/" .. name)
 
 		if remote then return remote end
@@ -194,9 +121,9 @@ function getRemote(name)
 	end
 end
 
-function remoteInvokeHandler(name, func)
+function onRemoteCall(name, func)
 	local remote = getRemote(name)
-	if Server then
+	if IS_SERVER then
 		remote.OnServerInvoke = func
 	else
 		remote.OnClientInvoke = func
@@ -221,7 +148,10 @@ end
 
 local instanceIDs = {}
 
-if Server then
+local dataReceivedSignal, fireDataReceived = Signal()
+Ambassador.DataReceived = dataReceivedSignal
+
+if IS_SERVER then
 
 	function getId(instance)
 		if instanceIDs[instance] then
@@ -234,13 +164,13 @@ if Server then
 		end
 	end
 
-	remoteInvokeHandler("RemoteRequestHandler", function(player, name)
-		return createRemote(name)
+	onRemoteCall("Transmission", function(player, name, data)
+		fireDataReceived(player, name, decodeTransmission(player, data))
 	end)
 
-	remoteInvokeHandler("FunctionCall", function(player, id, data)
+	onRemoteCall("FunctionCall", function(player, id, data)
 		local success, result = pcall(function()
-			return encodeTransmittion(player, objects[id](decodeTransmittion(player, data)))
+			return encodeTransmission(player, objects[id](decodeTransmission(player, data)))
 		end)
 
 		if success then
@@ -251,12 +181,12 @@ if Server then
 		end
 	end)
 
-	remoteInvokeHandler("GetID", function(player, instance)
+	onRemoteCall("GetID", function(player, instance)
 		if typeof(instance) ~= "Instance" then return "" end
 		return getId(instance)
 	end)
 
-	remoteInvokeHandler("GetInstance", function(player, id)
+	onRemoteCall("GetInstance", function(player, id)
 		if type(id) ~= "string" then return "" end
 		if instanceIDs[id] then
 			return instanceIDs[id]
@@ -267,8 +197,12 @@ if Server then
 
 else
 
-	remoteInvokeHandler("FunctionCall", function(id, data)
-		return encodeTransmittion(nil, objects[id](decodeTransmittion(nil, data)))
+	onRemoteCall("Transmission", function(name, data)
+		fireDataReceived(name, decodeTransmission(nil, data))
+	end)
+
+	onRemoteCall("FunctionCall", function(id, data)
+		return encodeTransmission(nil, objects[id](decodeTransmission(nil, data)))
 	end)
 
 end
@@ -336,7 +270,7 @@ function encode(player, ...)
 	elseif dataType == "Instance" then
 		local id
 		
-		if Server then
+		if IS_SERVER then
 			id = getId(data)
 		else
 			id = getRemote("GetID"):InvokeServer(data)
@@ -429,11 +363,11 @@ function decode(player, data)
 			return unpack(decoded, 1, data.length)
 		elseif dataType == "function" then
 			local result = function(...)
-				if Server then
+				if IS_SERVER then
 					local player = players[data.id]
-					return decodeTransmittion(player, getRemote("FunctionCall"):InvokeClient(player, data.id, encodeTransmittion(player, ...)))
+					return decodeTransmission(player, getRemote("FunctionCall"):InvokeClient(player, data.id, encodeTransmission(player, ...)))
 				else
-					return decodeTransmittion(nil, getRemote("FunctionCall"):InvokeServer(data.id, encodeTransmittion(nil, ...)))
+					return decodeTransmission(nil, getRemote("FunctionCall"):InvokeServer(data.id, encodeTransmission(nil, ...)))
 				end
 			end
 
@@ -444,7 +378,7 @@ function decode(player, data)
 		elseif dataType == "instance" then
 			local instance
 			
-			if Server then
+			if IS_SERVER then
 				instance = instanceIDs[data.id or ""]
 			else
 				instance = getRemote("GetInstance"):InvokeServer(data.id)
@@ -457,7 +391,7 @@ function decode(player, data)
 	error("Cannot decode '" .. dataType .. "'", 0)
 end
 
-function encodeTransmittion(to, ...)
+function encodeTransmission(to, ...)
 	local length = select("#", ...)
 	local encoded = {}
 
@@ -465,54 +399,38 @@ function encodeTransmittion(to, ...)
 		encoded[i] = encode(to, (select(i, ...)))
 	end
 
-	local result = {
+	return game:GetService("HttpService"):JSONEncode({
 		type = "vararg",
 		length = length,
 		value = encoded
-	}
-
-	return encrypt(game:GetService("HttpService"):JSONEncode(result), key)
+	})
 end
 
-function decodeTransmittion(from, data)
-	return decode(from, game:GetService("HttpService"):JSONDecode(decrypt(data, key)))
+function decodeTransmission(from, data)
+	return decode(from, game:GetService("HttpService"):JSONDecode(data))
 end
 
 function Ambassador:Send(name, target, data)
-	assert(not (Server and not data), "Expected target for server ambassador")
+	assert(not (IS_SERVER and not data), "Expected target for server ambassador")
 
-	if Server then
+	if IS_SERVER then
 		assert(target.Parent, "Expected target to be in-game")
 	else
 		data = target
 		target = game.Players.LocalPlayer
 	end
 
-	local remote = createRemote(getName(Server, name, target))
-
-	local function invokationHandler(whosRequesting, type, ...)
-		if type == InvokationType.RequestAmbassador then
-			return encodeTransmittion(whosRequesting, data)
-		else
-			error("Unknown request", 0)
-		end
-	end
-
-	if Server then
-		function remote.OnServerInvoke(player, ...)
-			return invokationHandler(player, ...)
-		end
+	if IS_SERVER then
+		getRemote("Transmission"):InvokeClient(target, name, encodeTransmission(target, data))
 	else
-		function remote.OnClientInvoke(...)
-			return invokationHandler(nil, ...)
-		end
+		getRemote("Transmission"):InvokeServer(name, encodeTransmission(nil, data))
 	end
 end
 
 function Ambassador:Await(name, target, timeout)
-	assert(not (Server and not target), "Expected target for client ambassador")
+	assert(not (IS_SERVER and not target), "Expected target for client ambassador")
 
-	if Server then
+	if IS_SERVER then
 		assert(target.Parent, "Expected target to be in-game")
 	else
 		timeout = target
@@ -520,23 +438,42 @@ function Ambassador:Await(name, target, timeout)
 	end
 
 	timeout = timeout or 30
+	
+	-- this should be improved code-wise
 
-	local remote
-	local start = tick()
-	repeat
-		remote = game:GetService("ReplicatedStorage"):FindFirstChild(getName(not Server, name, target))
+	local len, resultData
 
-		if tick() - start > timeout then
-			return false, "Timeout"
-		end
+	coroutine.wrap(function()
+		wait(timeout)
+		resultData = { false, "Timeout" }
+		len = 2
+	end)()
 
-		wait() until remote
+	coroutine.wrap(function()
+		local hasResult, result
+		repeat
+			if IS_SERVER then
+				local nextPlayer, nextName, nextData = dataReceivedSignal:Wait()
+				if nextPlayer == target and nextName == name then
+					result = nextData
+					hasResult = true
+				end
+			else
+				local nextName, nextData = dataReceivedSignal:Wait()
+				if nextName == name then
+					result = nextData
+					hasResult = true
+				end
+			end
 
-	if Server then
-		return true, decodeTransmittion(target, remote:InvokeClient(target, InvokationType.RequestAmbassador))
-	else
-		return true, decodeTransmittion(nil, remote:InvokeServer(InvokationType.RequestAmbassador))
-	end
+			wait() until hasResult or len
+		resultData = { true, result }
+		len = 2
+	end)()
+	
+	repeat wait() until len ~= nil
+
+	return unpack(resultData, 1, len)
 end
 
 function Ambassador:Receive(...)
@@ -545,7 +482,7 @@ function Ambassador:Receive(...)
 	return ambassador
 end
 
-if Server then
+if IS_SERVER then
 	function Ambassador:Cleanup(player)
 		local suffix = "/" .. player.Name
 		for i, v in ipairs(game:GetService("ReplicatedStorage"):GetChildren()) do
