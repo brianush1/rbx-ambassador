4ef3d6a87184923700d2aae0823edfb1a0d7153d
brianush1 <brianush1@outlook.com>
2019-02-18 11:52:23 -0500

    Create rbx-packman package.lua

---
+++ package.lua
@@ -0,0 +1,8 @@
+return {
+	name = "Ambassador",
+	version = "0.08",
+	description = "A library that greatly simplifies communication between server and client",
+	author = "brianush1",
+	license = "Apache 2.0",
+	dependencies = {}
+}
